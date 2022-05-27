2a97d952cba289eef4830f0f8daf32579a132e7e
VICTORIA GRAY <66696319+vgray2020@users.noreply.github.com>
2022-05-27 09:23:42 -0500

    done

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,6 +3,7 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -10,12 +11,17 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model)    {
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+    public String processAddUserForm(Model model, @ModelAttribute User user, Errors errors, String verify) {
+        if(errors.hasErrors() ) {
+            model.addAttribute("errors", errors);
+            return "user/add";
+        }
         model.addAttribute("user", user);
         model.addAttribute("verify", verify);
         model.addAttribute("username", user.getUsername());
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,8 +1,22 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "UserName cannot be blank.")
+    @Size(min=5, max=15, message = "Choose a username between 5 and 15 letters")
     private String username;
+
+    @Email
     private String email;
+
+
+    @NotBlank(message = "Password cannot be blank.")
+    @Size(min = 6, message = "Must be at least 6 letters")
     private String password;
 
     public User() {
